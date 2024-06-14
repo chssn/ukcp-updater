1f62b2176b10b7d157c6f76fb49280ecacfc4a32
chssn <33300267+chssn@users.noreply.github.com>
2024-06-14 21:59:00 +0100

    refactor: airac class

--- ukcp_updater/functions.py
+++ ukcp_updater/functions.py
@@ -9,11 +9,11 @@ Chris Parkinson (@chssn)
 import csv
 import ctypes
 import datetime
+import math
 import os
 import re
 import subprocess
 from getpass import getpass
-from math import floor
 from tkinter import Tk
 from tkinter import filedialog
 from packaging.version import Version
@@ -27,51 +27,52 @@ from loguru import logger
 # Local Libraries
 
 class Airac:
-    """Class for general functions relating to AIRAC"""
+    """
+    Class for general functions relating to AIRAC
+    """
 
     def __init__(self):
         # First AIRAC date following the last cycle length modification
-        start_date = "2019-01-02"
-        self.base_date = datetime.date.fromisoformat(str(start_date))
+        self.start_date = "2019-01-02"
+        self.base_date = datetime.date.fromisoformat(self.start_date)
         # Length of one AIRAC cycle
         self.cycle_days = 28
-        # Today
-        self.today_date = datetime.datetime.now().date()
 
-    def initialise(self, date_in:str=0) -> int:
+    def _initialise(self, date_in:str="") -> int:
         """Calculate the number of AIRAC cycles between any given date and the start date"""
-        if date_in:
+
+        if date_in != "":
             input_date = datetime.date.fromisoformat(str(date_in))
         else:
             input_date = datetime.date.today()
 
         # How many AIRAC cycles have occured since the start date
         diff_cycles = (input_date - self.base_date) / datetime.timedelta(days=1)
+        logger.debug(f"{diff_cycles} days have passed since the start date ({self.start_date})")
         # Round that number down to the nearest whole integer
-        number_of_cycles = floor(diff_cycles / self.cycle_days)
-        logger.debug(f"{number_of_cycles} AIRAC cycles since {self.base_date}")
+        number_of_cycles = math.floor(diff_cycles / self.cycle_days)
+        logger.debug(
+            f"{diff_cycles} divided by {self.cycle_days} (AIRAC cycle length) and rounded down is "
+            f"{number_of_cycles} AIRAC cycles")
 
         return number_of_cycles
 
-    def current_cycle(self) -> str:
+    def cycle(self, next_cycle:bool=False, date_in:str="") -> datetime.date:
         """Return the date of the current AIRAC cycle"""
-        def cycle(sub:int=0):
-            number_of_cycles = self.initialise() - sub
+
+        number_of_cycles = self._initialise(date_in)
+        if next_cycle:
+            number_of_days = (number_of_cycles + 1) * self.cycle_days + 1
+        else:
             number_of_days = number_of_cycles * self.cycle_days + 1
-            current_cycle = self.base_date + datetime.timedelta(days=number_of_days)
-            return current_cycle
+        select_cycle = self.base_date + datetime.timedelta(days=number_of_days)
+        logger.success(f"The selected AIRAC cycle date is: {select_cycle}")
 
-        current_cycle = cycle()
-        if current_cycle > self.today_date:
-            current_cycle = cycle(sub=1)
-
-        logger.info("Current AIRAC Cycle is: {}", current_cycle)
-
-        return str(current_cycle)
+        return select_cycle
 
     def current_tag(self) -> str:
         """Returns the current tag for use with git"""
-        current_cycle = self.current_cycle()
+        current_cycle = self.cycle()
         # Split the current_cycle by '-' and return in format yyyy/mm
         split_cc = str(current_cycle).split("-")
         logger.debug(f"Current tag should be {split_cc[0]}/{split_cc[1]}")
